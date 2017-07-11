44e28e28ff725a4eb63c206b978d5f2285c74947
jb-reefer <james.reefer@gmail.com>
2017-07-11 18:12:28 -0400

    restart

---
+++ Paiger/Models/ArticleModel.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Paiger.Models
+{
+    public class ArticleModel
+    {
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public Uri Link { get; set; }
+
+        [Required]
+        [MaxLength(100)]
+        public string Genre { get; set; }
+
+        [Required]
+        [MinLength(5)]
+        [MaxLength(50)]
+        public string Publisher { get; set; }
+
+        [Required]
+        public DateTime DatePublished { get; set; }
+
+
+    }
+}
